7b131366aece69de873e977fc1d1a359e7ef31e9
singhshubham593 <92996831+singhshubham593@users.noreply.github.com>
2025-07-02 14:13:34 +0530

    personal data

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,21 +1,69 @@
 import React from 'react'
+import App from '../App';
 
-const Personal = () => {
+const Personal = ({data,setData,errors}) => {
+   const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
   return (
-    <div className='mb-10'>
-        <div className=''>
-        <label htmlFor='name'>Name:</label>
-        <input type="text" placeholder='Enter name' name='name' className='bg-gray-200 '/>
+    <div className="m-10 bg-blue-100 rounded-2xl">
+          <div className="p-5">
+            <label htmlFor="name" className="mx-3">
+              Name :
+            </label>
+            <input
+              type="text"
+              placeholder="Enter name"
+              name="name"
+              value={data.name}
+              onChange={handleChange}
+              className={`bg-gray-200 border p-2 rounded-2xl ${
+                errors.name ? "border-red-600" : "border-black"
+              }`}
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm">{errors.name}Hello</p>
+            )}
+          </div>
+
+          <div className="p-4">
+            <label htmlFor="email" className="mx-3">
+              Email :{" "}
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              className={`bg-gray-200 border p-2 rounded-2xl ${
+                errors.email ? "border-red-600" : "border-black"
+              }`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email} Hello</p>
+            )}
+          </div>
+          <div className="p-4">
+            <label htmlFor="phone" className="mx-3">
+              Phone :{" "}
+            </label>
+            <input
+              text="text"
+              Placeholder="Enter phone number"
+              name="phone"
+              value={data.phone}
+              onChange={handleChange}
+              className={`bg-gray-200 border p-2 rounded-2xl ${
+                errors.phone ? "border-red-600" : "border-black"
+              }`}
+            />
+            {errors.phone && (
+              <p className="text-red-500 text-sm">{errors.phone}</p>
+            )}
+          </div>
         </div>
-        <div>
-        <label htmlFor='name'>Email No:</label>
-        <input type="text" placeholder='Enter Email' className='bg-gray-200'/>
-        </div>
-        <div>
-        <label htmlFor='name'>Phone No:</label>
-        <input text='text' Placeholder='Enter phone number' className='bg-gray-200'/>
-        </div>
-    </ div>
   )
 }
 
